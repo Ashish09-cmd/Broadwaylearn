d59d07a265f34e9d365584ab16a2b4d830a85af9
Ashish09-cmd <actharu28@gmail.com>
2025-12-26 17:06:11 +0545

    folder structure and complete header and footer

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,211 @@
+import { Icon } from "@iconify/react";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer-section">
+        <div className="bg-text-heading ">
+          <div className="container section-padding border-b border-darker-border">
+            <div className="flex flex-col gap-6">
+              <a href="/">
+                <img src="./flogo.png" alt="" className="h-13.5" />
+              </a>
+              <nav>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-8">
+                <div className="md:col-span-5 ">
+                  <p className="text-sm font-regular mb-4 text-gray-300 line-height-sm">
+                    IT training company dedicated to delivering practical,
+                    industry-focused technology education to build job-ready
+                    professionals.
+                  </p>
+                  <ul className="flex flex-col gap-2">
+                    <li>
+                      <a
+                        href=""
+                        className="flex items-center gap-2 text-gray-300"
+                      >
+                        <div className="social-icon">
+                          <Icon icon="typcn:location"></Icon>
+                        </div>
+                        Shree Ganesh Marg, Subidhanagar, Tinkune, Kathmandu
+                        44600, Nepal
+                      </a>
+                    </li>
+                    <li className="flex items-center gap-2">
+                      <div>
+                        <Icon
+                          icon="entypo:old-phone"
+                          className="text-white"
+                        ></Icon>
+                      </div>
+                      <div className="text-sm font-regular text-gray-300 ">
+                        <a
+                          href="tel:+977-1-4111849"
+                          className="text-sm font-regular text-gray-300"
+                        >
+                          +977-1-4111849
+                        </a>
+                        <span> / </span>
+                        <a
+                          href="tel:+4117578"
+                          className="text-sm font-regular text-gray-300"
+                        >
+                          4117578
+                        </a>
+                        <span> / </span>
+                        <a
+                          href="tel:+4111583"
+                          className="text-sm font-regular text-gray-300"
+                        >
+                          4111583
+                        </a>
+                        <span> / </span>
+                        <a
+                          href="tel:+5912405"
+                          className="text-sm font-regular text-gray-300"
+                        >
+                          5912405
+                        </a>
+                      </div>
+                    </li>
+                    <li>
+                        <a
+                          href="mailto:info@broadwaylearn.com"
+                          className="text-sm text-gray-300 font-regular flex items-center gap-2"
+                        >
+                          <Icon icon="ic:round-email"></Icon>
+                          info@broadwaylearn.com
+                        </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="md:col-span-7">
+                  <div className="grid grid-cols-1 md:grid-cols-3">
+                    <div className="flex flex-col gap-2">
+                      <h6 className="font-geologica text-md font-bold text-white line-height-md">
+                        Company
+                      </h6>
+                      <ul className="flex flex-col gap-2 ">
+                        <li>
+                          <a
+                            href=""
+                            className="text-white font-regular text-sm"
+                          >
+                            About Us
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href=""
+                            className="text-white font-regular text-sm"
+                          >
+                            Our Services
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href=""
+                            className="text-white font-regular text-sm"
+                          >
+                            Career
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href=""
+                            className="text-white font-regular text-sm"
+                          >
+                            Contact us
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <h6 className="font-geologica text-md font-bold text-white line-height-md">
+                        For Students
+                      </h6>
+                      <ul className="flex flex-col gap-2 ">
+                        <li>
+                          <a
+                            href=""
+                            className="text-white font-regular text-sm"
+                          >
+                            Enroll Now
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href=""
+                            className="text-white font-regular text-sm"
+                          >
+                            Offers
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href=""
+                            className="text-white font-regular text-sm"
+                          >
+                            Students Testimonials
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href=""
+                            className="text-white font-regular text-sm"
+                          >
+                            Certificate Verifications
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <h6 className="font-geologica text-md font-bold text-white line-height-md">
+                        Resources
+                      </h6>
+                      <ul className="flex flex-col gap-2 ">
+                        <li>
+                          <a
+                            href=""
+                            className="text-white font-regular text-sm"
+                          >
+                            Blog
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href=""
+                            className="text-white font-regular text-sm"
+                          >
+                            FAQs
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href=""
+                            className="text-white font-regular text-sm"
+                          >
+                            Sitemap
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </nav>
+            </div>
+          </div>
+          <div className="sub-footer">
+            <div className="container py-6">
+                <p className="text-center text-sm font-regular text-gray-500 ">A product of Broadway Infosys. © 2025 Broadway Learn. All rights reserved.</p>
+            </div>
+         </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,407 @@
+"use client";
+import { useState, useEffect } from "react";
+import { Icon } from "@iconify/react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
+  const [isCoursesDropdownOpen, setIsCoursesDropdownOpen] = useState(false);
+  const [isSubDropdownOpen, setIsSubDropdownOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    if (!isMenuOpen) {
+      document.body.classList.add("menu-open");
+    } else {
+      document.body.classList.remove("menu-open");
+    }
+  };
+
+  const toggleCoursesDropdown = () => {
+    setIsCoursesDropdownOpen(!isCoursesDropdownOpen);
+  };
+
+  const toggleSubDropdown = () => {
+    setIsSubDropdownOpen(!isSubDropdownOpen);
+  };
+
+  return (
+    <>
+      <header
+        className={`hidden lg:block ${
+          isSticky ? "sticky-header" : ""
+        } border-b border-[#0000001A] `}
+      >
+        <div className={`container ${isSticky ? "py-5" : "py-5"}`}>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-6">
+              <div>
+                <Link href={"/"} className="logo">
+                  <img src="./logo.png" alt="" />
+                </Link>
+              </div>
+              <div className="flex-1">
+                <div className="flex justify-between items-center">
+                  <div className="flex items-center gap-4">
+                    <div className="relative dropdown-menu ">
+                      <button
+                        onClick={toggleCoursesDropdown}
+                        className="flex items-center gap-1 px-4 py-3 text-sm font-medium font-googleSansFlex text-text-subtext cursor-pointer"
+                      >
+                        Explore
+                        <Icon
+                          icon="meteor-icons:angle-down"
+                          className="text-text-subtext"
+                        ></Icon>
+                      </button>
+                      <div className="p-6 dropdowpn-megamenu invisible bg-white shadow-xl border border-gray-border absolute top-20 w-200 z-30 -left-40">
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 ">
+                          <div>
+                            <h5 className="text-md font-semibold text-gray-text-title mb-2 font-poppins">
+                              Popular Courses
+                            </h5>
+                            <ul className="flex flex-col gap-2">
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  Dale Carnegie Course
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  Develop you leadership
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  High Impact Presentations
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  Winning with realtionship
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
+                          <div>
+                            <h5 className="text-md font-semibold text-gray-text-title mb-2 font-poppins">
+                              Course Topics
+                            </h5>
+                            <ul className="flex flex-col gap-2">
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  Leadership
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  Presentation & Public Speaking
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  People Skills
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  Sales
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  customer Services
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  Organizational Development
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  Youth & Teen
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
+                          <div>
+                            <h5 className="text-md font-semibold text-gray-text-title mb-2 font-poppins">
+                              Course Types
+                            </h5>
+                            <ul className="flex flex-col gap-2">
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  In Person
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  Live Online
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  Online Subscription
+                                </a>
+                              </li>
+                              <li>
+                                <a
+                                  href="#"
+                                  className="text-sm font-regular text-gray-text-body"
+                                >
+                                  On Demand
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <form
+                      action="#"
+                      method="GET"
+                      className="flex items-center relative rounded-[38px] border-1 border-[#DADADC] px-4 py-3 justify-between"
+                    >
+                      <input
+                        type="text"
+                        placeholder="Search for the courses"
+                        id="searchInput"
+                        className="text-sm border-none  outline-0 w-full"
+                      />
+                      <div className="searchIcon" id="search-icon"></div>
+                      <Icon
+                        icon="ri:search-line"
+                        className="absolute right-4 text-gray-400 text-lg cursor-pointer"
+                      />
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center gap-6">
+              <p className="text-sm font-regular font-googleSansFlex text-text-subtext">Teach With Us </p>
+              <button className="cursor-pointer py-2 px-4 rounded-lg border text-sm font-regular font-googleSansFlex border-primary text-primary ">
+                Log In
+              </button>
+              <button className="cursor-pointer py-2 px-4 rounded-lg text-sm font-regular font-googleSansFlex bg-primary text-white ">Sign Up</button>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      {/* Mobile Header */}
+      <header
+        className={`lg:hidden bg-white border-b border-[#DADADC] py-4 px-4 ${
+          isSticky ? "sticky-header" : ""
+        }`}
+      >
+        <div className="flex items-center justify-between">
+          <div>
+            <Link href={"/"}>
+              <img src="./logo.png" alt="Logo" className="h-8.5  w-auto" />
+            </Link>
+          </div>
+          <div className="flex items-center gap-3">
+            <div className="relative">
+              <button
+                onClick={toggleCoursesDropdown}
+                className="flex items-center text-vxs md:text-xs gap-2 px-3 py-2  font-medium bg-blue-500 text-white "
+              >
+                EXPLORE COURSES
+                <Icon icon="flowbite:angle-down-outline"></Icon>
+              </button>
+              {isCoursesDropdownOpen && (
+                <div className="absolute top-full left-0 mt-2 bg-white border border-gray-200 rounded shadow-lg z-50 w-64">
+                  <div className="p-4">
+                    <h3 className="font-semibold text-gray-800 mb-2">
+                      Course Categories
+                    </h3>
+                    <ul className="space-y-2">
+                      <li>
+                        <button
+                          onClick={toggleSubDropdown}
+                          className="flex items-center justify-between w-full text-left text-gray-700 hover:text-blue-600"
+                        >
+                          <span>Technology</span>
+                          <Icon
+                            icon="flowbite:angle-down-outline"
+                            className={`transform transition-transform ${
+                              isSubDropdownOpen ? "rotate-180" : ""
+                            }`}
+                          ></Icon>
+                        </button>
+                        {isSubDropdownOpen && (
+                          <ul className="mt-2 ml-4 space-y-1">
+                            <li>
+                              <Link
+                                href="/courses/web-development"
+                                className="block text-sm text-gray-600 hover:text-blue-600"
+                              >
+                                Web Development
+                              </Link>
+                            </li>
+                            <li>
+                              <Link
+                                href="/courses/mobile-development"
+                                className="block text-sm text-gray-600 hover:text-blue-600"
+                              >
+                                Mobile Development
+                              </Link>
+                            </li>
+                            <li>
+                              <Link
+                                href="/courses/data-science"
+                                className="block text-sm text-gray-600 hover:text-blue-600"
+                              >
+                                Data Science
+                              </Link>
+                            </li>
+                          </ul>
+                        )}
+                      </li>
+                      <li>
+                        <Link
+                          href="/courses/design"
+                          className="block text-gray-700 hover:text-blue-600"
+                        >
+                          Design
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          href="/courses/business"
+                          className="block text-gray-700 hover:text-blue-600"
+                        >
+                          Business
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              )}
+            </div>
+            <button>
+              <Icon
+                icon="uil:search"
+                className="text-lg text-gray-text-body"
+              ></Icon>
+            </button>
+            <button onClick={toggleMenu}>
+              <Icon
+                icon="ic:baseline-menu"
+                className="text-xl text-gray-text-body"
+              ></Icon>
+            </button>
+          </div>
+        </div>
+      </header>
+
+      {/* Mobile Menu Sidebar */}
+      <div
+        className={`fixed top-0 right-0 h-full bg-white z-50 transition-transform duration-300 ${
+          isMenuOpen ? "translate-x-0" : "translate-x-full"
+        } w-4/5 md:w-2/5 `}
+      >
+        <div className="p-4">
+          <button
+            onClick={toggleMenu}
+            className="mb-4 text-end w-full flex justify-end"
+          >
+            <Icon
+              icon="uil:multiply"
+              className="text-xl text-gray-text-body "
+            ></Icon>
+          </button>
+          <ul className="space-y-4">
+            <li>
+              <Link
+                href="/upcoming-classes"
+                className="text-gray-700 font-medium"
+              >
+                UPCOMING CLASSES
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="text-gray-700 font-medium">
+                EVENTS & NOTICES
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="text-gray-700 font-medium">
+                CONTACT
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Overlay */}
+      {isMenuOpen && (
+        <div
+          className="fixed inset-0 bg-black/50 bg-opacity-80 z-40"
+          onClick={toggleMenu}
+        ></div>
+      )}
+    </>
+  );
+};
+
+export default Header;
